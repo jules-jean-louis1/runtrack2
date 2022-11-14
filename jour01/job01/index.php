ed0d43cfb535f96c5102884fff77e6bfc7d33662
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2022-11-14 10:44:37 +0100

    add file

---
+++ jour01/job01/index.php
@@ -0,0 +1,25 @@
+$str="La Plateforme"
+
+
+
+
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="job01-php.css">
+    <title>Doc PHP</title>
+</head>
+<body>
+    <main>
+        <section>
+            <div class="job01_test">
+                <?php
+                $str="La Plateforme"
+                ?>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
