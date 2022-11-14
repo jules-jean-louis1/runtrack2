dba4fd4d61d83ef5b4b97e962cb8407d308c0ceb
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2022-11-14 14:36:06 +0100

    ajout job02 et job03

---
+++ jour01/job02/index.php
@@ -0,0 +1,11 @@
+<?php
+$bonj="Hello LaPlateforme!";
+$bonj2="<br>" . "Hello" . "<br>" . "LaPlateforme". "<br>" . "!";
+?>
+
+<p>
+    <?php 
+        echo $bonj;
+        echo $bonj2;
+    ?>
+</p>
---
+++ jour01/job03/index.php
@@ -0,0 +1,58 @@
+<?php
+$noteTab = [1,2,5,9];
+$nVirguleTab = ['9,48','14,50','18,66','50,51'];
+$motTab = ['Bonjour' , 'tous' , 'le' , 'monde' , '!'];
+$myBool =[true,false]
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="job03.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <table>
+            <tr>
+                <th>Nom</th>
+                <th>Âge</th>
+                <th>Pays</th>
+            </tr>
+
+            <tr>
+                <td>Carmen</td>
+                <td>33 ans</td>
+                <td>Espagne</td>
+            </tr>
+            <tr>
+                <td>Michelle</td>
+                <td>26 ans</td>
+                <td>États-Unis</td>
+            </tr>
+        </table>
+    </header>
+    <main>
+        <section>
+
+        </section>
+    </main>
+</body>
+</html>
+
+<!-- $note='1';
+$note2='2';
+$note3='3';
+$note4='4';
+$nVirgule='10,45';
+$nVirgule2='14,50';
+$nVirgule3='18,66';
+$mot='bonjour';
+$mot2='le';
+$mot3='monde';
+$mot4='!';
+$myBool=true;
+$myBool2='false'
+ -->
