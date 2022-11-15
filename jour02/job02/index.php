5767155c292d3fa90abd23469b84fe5016669e52
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2022-11-15 12:00:56 +0100

    job2

---
+++ jour02/job02/index.php
@@ -0,0 +1,17 @@
+<?php
+for( $i=0; $i<=1337; $i++ )
+{ if ($i === 26) {
+	echo "<br>";
+} if ($i === 37) {
+	echo "<br>";
+} if ($i === 88) {
+	echo "<br>";
+} if ($i === 1111) {
+	echo "<br>";
+} if ($i === 3233) {
+	echo "<br>"; 
+} else
+echo $i;
+echo "<br>";
+}
+?>
