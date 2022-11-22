e64cda7560f7ef8bd1f6278194837529c8f9b333
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2022-11-22 16:19:04 +0100

    job06

---
+++ jour07/job07/index.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+
+?>
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job07</title>
+</head>
+<body>
+    <main>
+        <form action="index.php" method="post">
+            <select name="select" id="">
+                <option value="" name="gars">Gras</option>
+                <option value="" name="maj">Majuscule</option>
+                <option value="" name="underline">Souligner</option>
+                <input type="submit" value="envoyer" name="ok">
+            </select>
+        </form>
+    </main>
+</body>
+</html>
